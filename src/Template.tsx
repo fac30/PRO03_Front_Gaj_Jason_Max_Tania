79c89f6c18f913e9dbda06cff050424b9431c9c2
Jason Warren <contact.jwarren@gmail.com>
2024-09-30 17:31:45 +0100

    template component & react types

---
+++ src/Template.tsx
@@ -0,0 +1,12 @@
+//import statements
+
+function Template() {
+  return (
+    <div>
+			<p>Content of the div</p>
+		</div>
+  )
+}
+
+// separate export statement, so it is easy to see at a glance
+export default Template;
