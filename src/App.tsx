71e5a384609ce0d1e78a1e2cff7fcac41fb58575
maxitect <maxime.downe@gmail.com>
2024-10-01 14:25:20 +0100

    chore: initial button

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,17 @@ import './App.css'
 import Header from './sections/Header'
 import Content from './sections/Content'
 import Footer from './sections/Footer'
+import Button from './buttons/Button';
+//import RoundedButton from './buttons/Button'
 
 function App() {
   return (
 		<>
 			<Header />
 			<Content />
+			<Button onClick={function (): void {
+			  throw new Error('Function not implemented.')
+		  } } label={'Click me'} />
 			<Footer />
 		</>
   );
