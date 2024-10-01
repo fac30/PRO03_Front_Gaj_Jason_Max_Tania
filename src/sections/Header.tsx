5088f596108c8e4c621b8cb18c6eee36c30dbe33
GurTati <118075413+gurtatiLND@users.noreply.github.com>
2024-10-01 15:03:51 +0100

    changes in navbar

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -1,13 +1,23 @@
-//import statements
+import React from "react";
+
 
 // declare the component with a capitalised name
-function Header() {
+const Header: React.FC = () => {
   return (
-    <header>
-				<h1>
-					MoodTime
-				</h1>
-		</header>
+    <nav>
+		<div>
+		    <img 
+			    src="./assets/logo-background.png"
+				width="60"
+				height="60"
+				alt="logo of the app"
+			/>
+		    <span>SpotifyMoodList</span>
+		    <h1>
+			    MoodTime
+		    </h1>
+		</div>		
+	</nav>
   )
 }
 
