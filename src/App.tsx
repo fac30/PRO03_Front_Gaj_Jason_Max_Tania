89275a0e0713fdc2be27dcf736ea58f943f407a7
GurTati <118075413+gurtatiLND@users.noreply.github.com>
2024-10-01 16:20:52 +0100

    fix_2 for tailwind styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
-import './App.css'
-import Header from './sections/Header'
-import Content from './sections/Content'
+import './App.css';
+import Header from './sections/Header';
+import Content from './sections/Content';
 import Footer from './sections/Footer';
+import Button from './buttons/Button';
 
 function App() {
   return (
