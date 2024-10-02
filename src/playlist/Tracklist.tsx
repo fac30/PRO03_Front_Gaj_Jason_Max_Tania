af9d0ade3b8aa52e77e8781556aa793eebdda51e
maxitect <maxime.downe@gmail.com>
2024-10-02 18:21:33 +0100

    feat: Track and Tracklist components created to set up playlist with dummy data

--- src/playlist/Tracklist.tsx
+++ src/playlist/Tracklist.tsx
@@ -1,19 +1,15 @@
+// Tracklist.tsx
 import Track from './Track';
-import { TracklistProps } from '../utils/playlist';
+import { TrackProps, TracklistProps } from '../utils/playlist';
 
-
-function Playlist(playlist: TracklistProps) {
+function Tracklist({ playlist }: { playlist: TracklistProps }) {  // Fix prop structure
   return (
-    <div>
-      <h2>Playlist</h2>
-      {playlist.map((item) => (
-        <div key={item.id}>
-          <h3>{item.title}</h3>
-          <Track trackId={item.id} />
-        </div>
+    <>
+      {playlist.map((item: TrackProps) => (
+          <Track {...item} />
       ))}
-    </div>
+    </>
   );
-};
+}
 
-export default Playlist;
+export default Tracklist;
