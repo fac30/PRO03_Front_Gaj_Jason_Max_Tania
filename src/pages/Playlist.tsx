6480c476027cc504a62f3f8b1c2eec69adfdcd68
maxitect <maxime.downe@gmail.com>
2024-10-02 14:49:01 +0100

    chore: add Tracklist into Playlist page component

--- src/pages/Playlist.tsx
+++ src/pages/Playlist.tsx
@@ -1,4 +1,7 @@
 import Button from '../buttons/Button';
+import playlist from '../data/playlist.json';
+import Tracklist from '../playlist/Tracklist';
+import { TracklistProps } from '../utils/playlist';
 
 interface PlaylistProps {
 	onNext: () => void;
@@ -8,9 +11,20 @@ function Playlist({ onNext }: PlaylistProps) {
   return (
     // render background image that fills the whole component
 		<>
+            <div className="App">
+                <h1>My Spotify Playlist</h1>
+                {/* Pass playlistData to the Playlist component */}
+                <Tracklist playlist={playlist} />
+            </div>
+            <Button
+                onClick={() => {onNext}}
+                label="Save playlist"
+                disabled={false}
+                loading={false}
+            />
 			<Button
                 onClick={() => {onNext}}
-                label="Go again"
+                label="Try again"
                 disabled={false}
                 loading={false}
             />
