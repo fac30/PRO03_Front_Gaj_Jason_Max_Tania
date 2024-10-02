8f6d282ce4aa429e371e85087821ffe300cf4f50
maxitect <maxime.downe@gmail.com>
2024-10-02 18:21:47 +0100

    chore: Tracklist added to Playlist page

--- src/pages/Playlist.tsx
+++ src/pages/Playlist.tsx
@@ -1,3 +1,4 @@
+// Playlist.tsx
 import { useContext } from 'react';
 import Button from '../buttons/Button';
 import { UserContext } from '../sections/Content';
@@ -6,34 +7,36 @@ import Tracklist from '../playlist/Tracklist';
 import { TracklistProps } from '../utils/playlist';
 
 interface PlaylistProps {
-	onNext: () => void;
+  onNext: () => void;
 }
 
 function Playlist({ onNext }: PlaylistProps) {
-	const { userName } = useContext(UserContext);
+  const { userName } = useContext(UserContext);
 
-	return (
-		<>
-			<h2>Welcome, {userName}!</h2>
-            <div className="App">
-                <h1>My Spotify Playlist</h1>
-                {/* Pass playlistData to the Playlist component */}
-                <Tracklist playlist={playlist} />
-            </div>
-            <Button
-                onClick={() => {onNext}}
-                label="Save playlist"
-                disabled={false}
-                loading={false}
-            />
-			<Button
-                onClick={() => {onNext}}
-                label="Try again"
-                disabled={false}
-                loading={false}
-            />
-		</>
-	)
+  return (
+    <section id="playlist" className="bg-[var(--purple)] flex flex-col justify-center items-center">
+      <h1>{userName}, this is your</h1>
+      <h1 className="text-white">TOP 5</h1>
+      <div className="flex flex-col items-center w-3/4 h-96 overflow-y-auto shadow-xl shadow-[var(--purple-shadow)] bg-[var(--pink)] rounded-lg p-2">
+        {/* Correctly pass playlist data */}
+        <Tracklist playlist={playlist as TracklistProps} />
+      </div>
+      <div className="flex justify-center space-x-4 mt-4">
+      <Button
+        onClick={onNext}  // Correct invocation
+        label="Save playlist"
+        disabled={false}
+        loading={false}
+      />
+      <Button
+        onClick={onNext}  // Correct invocation
+        label="Try again"
+        disabled={false}
+        loading={false}
+      />
+      </div>
+    </section>
+  );
 }
 
 export default Playlist;
