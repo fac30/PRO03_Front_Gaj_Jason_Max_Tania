dcb27382f94e5691a16debc3166a3615e96d2f1a
Jason Warren <contact.jwarren@gmail.com>
2024-10-08 16:16:53 +0100

    feat(frontend tests): create skeleton test list

--- cypress/e2e/frontend.cy.ts
+++ cypress/e2e/frontend.cy.ts
@@ -1,9 +1,33 @@
 describe('Front-end Tests', () => {
-  beforeEach(() => {
-    cy.visit('/')
-  })
+  beforeEach(() => { 
+		cy.visit('http://localhost:5173')
+	})
 
-  it('', () => {
-    cy.get('')
-  })
+	context ('Landing Page', () => {
+		it('App does not advance with an empty name', () => {})
+		it('App does not advance with an offensive name', () => {})
+		it('App does not advance with an unsafe name', () => {})
+		it('App advances to Input Page with a valid name', () => {})
+	})
+
+	context ('Input Page', () => {
+		it('App does not advance with an empty date', () => {})
+		it('App does not advance with an empty genre', () => {})
+		it('App does not advance with an empty mood', () => {})
+		it('App makes API Call with a valid date, genre, and mood', () => {})
+	})
+
+	context ('API Call', () => {
+		it('App advances to Playlist Page with a valid API response', () => {})
+		it('Making the API call triggers the loading animation', () => {})
+	})
+
+	context ('Playlist Page', () => {
+		it('App displays the playlist title', () => {})
+		it('App displays tracks appropriate to the date, genre, and mood', () => {})
+		it('App displays a button to generate a new playlist', () => {})
+		it('App advances to Input Page when the "New Playlist" button is clicked', () => {})
+		it('App displays the correct number of tracks', () => {})
+		it('Individual tracks are playable', () => {})
+	})
 })
