ab8c8acde8c674d2681acabbc39eb373de726b41
Jason Warren <contact.jwarren@gmail.com>
2024-10-09 14:02:11 +0100

    build(cypress): types & structure
    - install cypress types
    - reference types in `frontend.cy.ts`
    - create sub contexts for fail states

--- cypress/e2e/frontend.cy.ts
+++ cypress/e2e/frontend.cy.ts
@@ -1,20 +1,34 @@
+/// <reference types="cypress" />
+
 describe('Front-end Tests', () => {
   beforeEach(() => { 
 		cy.visit('http://localhost:5173')
 	})
 
 	context ('Landing Page', () => {
-		it('App does not advance with an empty name', () => {})
-		it('App does not advance with an offensive name', () => {})
-		it('App does not advance with an unsafe name', () => {})
-		it('App advances to Input Page with a valid name', () => {})
+		context ('Name Input', () => {
+			context ('Failure States', () => {
+				it('App does not advance with an empty name', () => {})
+				it('App does not advance with an offensive name', () => {})
+				it('App does not advance with an unsafe name', () => {})
+			})
+			context ('Success States', () => {
+				it('App advances to Input Page with a valid name', () => {})
+			})
+		})
 	})
 
 	context ('Input Page', () => {
-		it('App does not advance with an empty date', () => {})
-		it('App does not advance with an empty genre', () => {})
-		it('App does not advance with an empty mood', () => {})
-		it('App makes API Call with a valid date, genre, and mood', () => {})
+		context ('Form Submission', () => {
+			context ('Failure States', () => {
+				it('App does not advance with an empty date', () => {})
+				it('App does not advance with an empty genre', () => {})
+				it('App does not advance with an empty mood', () => {})
+			})
+			context ('Success States', () => {
+				it('App makes API Call with a valid date, genre, and mood', () => {})
+			})
+		})
 	})
 
 	context ('API Call', () => {
