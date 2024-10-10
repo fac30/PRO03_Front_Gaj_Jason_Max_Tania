3182d96a737974dcaaa520a9d266e8f4eb3136b9
Tatiana G <118075413+gurtatiLND@users.noreply.github.com>
2024-10-10 11:48:27 +0100

    Update src/text/LandingText.tsx
    Co-authored-by: Jason Warren <contact.jwarren@gmail.com>

--- src/text/LandingText.tsx
+++ src/text/LandingText.tsx
@@ -1,7 +1,7 @@
 function LandingText() {
 	return (
 		<div className="text-[#383050] max-lg:text-lg max-lg:text-left max-lg:px-2 max-lg:py-2 px-8 py-3 text-xl flex flex-col items-center justify-center text-center max-w-lg mx-4 mb-5">
-			<h2>Discover your perfect playlist with our app, where your mood, emotions, and music preferences come together to create a personalized listening experience.</h2>
+			<p>Discover your perfect playlist with our app, where your mood, emotions, and music preferences come together to create a personalized listening experience.</p>
 		</div>
 	)
 }
