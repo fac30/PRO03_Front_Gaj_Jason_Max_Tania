94787737a21060022c4c15c344e111e5d0864804
maxitect <maxime.downe@gmail.com>
2024-12-06 19:09:52 +0000

    chore: updates to endpoint access on frontend

--- src/utils/fetchPlaylist.ts
+++ src/utils/fetchPlaylist.ts
@@ -6,19 +6,35 @@ export async function fetchPlaylist(
   if (userResponse) {
     try {
       // change back to the ip address!!!!
-      const moodTimeUrl = `http://35.176.251.92:3000/api/run/?date=${encodeURIComponent(
+
+      const moodTimeUrl = `http://35.176.251.92:3000/api/spotify/playlist/?date=${encodeURIComponent(
         userResponse.date
-      )}T00:00:00.000Z&eventDescription=${encodeURIComponent(
-        userResponse.eventDescription
-      )}&musicGenre=${encodeURIComponent(
+      )}T00:00:00.000Z&mood=Happy&genre=${encodeURIComponent(
         userResponse.musicGenre
-      )}&playlistCount=${encodeURIComponent(userResponse.playlistCount)}`;
+      )}&playlistCount=${encodeURIComponent(
+        userResponse.playlistCount
+      )}&valence=0.9&energy=0.8&danceability=0.8&acousticness=0.1&tempo=120`;
+
       const response = await fetch(moodTimeUrl, {
         method: "GET",
         headers: {
           "Content-Type": "application/json",
         },
       });
+
+      // const moodTimeUrl = `http://35.176.251.92:3000/api/run/?date=${encodeURIComponent(
+      //   userResponse.date
+      // )}T00:00:00.000Z&eventDescription=${encodeURIComponent(
+      //   userResponse.eventDescription
+      // )}&musicGenre=${encodeURIComponent(
+      //   userResponse.musicGenre
+      // )}&playlistCount=${encodeURIComponent(userResponse.playlistCount)}`;
+      // const response = await fetch(moodTimeUrl, {
+      //   method: "GET",
+      //   headers: {
+      //     "Content-Type": "application/json",
+      //   },
+      // });
 
       if (!response.ok) {
         throw new Error("Network response was not ok");
